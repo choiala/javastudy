8dea8e2f5d92704e2a14220dab7ccd7cf87bbb25
choiala <73679392+choiala@users.noreply.github.com>
2020-12-15 19:41:23 +0900

    Create Exam01.java

---
+++ ch03/src/sec05/Exam01.java
@@ -0,0 +1,14 @@
+package sec05;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		// 삼항(조건)연산식
+		
+		int score = 80;
+		char grade = (score >= 90) ? 'A':'B' ;
+		System.out.println("grade:" + grade);
+
+	}
+
+}
