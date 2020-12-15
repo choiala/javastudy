deb5ab3c178d5758454af5841238c865988d012c
choiala <73679392+choiala@users.noreply.github.com>
2020-12-15 19:41:00 +0900

    Create Exam01.class

---
+++ ch02/src/sec01/Exam01.java
@@ -0,0 +1,16 @@
+package sec01;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		//변수 선언
+		byte var1;
+		//변수에 값을 저장
+		var1 = 10;
+		System.out.println("var1:" + var1);
+		//선언과 동시에 값을 저장
+		byte var2 = 100; //자료형 변수명 = 초기값; '=' : 오른쪽 값을 왼쪽에 넣어라
+		System.out.println("var2:" + var2);
+	}
+
+}
