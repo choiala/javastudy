c5548866695b01f8ec25479b6a1c5a728b27bbbb
choiala <73679392+choiala@users.noreply.github.com>
2020-12-16 17:32:08 +0900

    Create Exam08.java

---
+++ ch05/src/sec06/Exam08.java
@@ -0,0 +1,19 @@
+package sec06;
+
+public class Exam08 {
+
+	public static void main(String[] args) {
+		
+		int[][] scores = new int[2][3];
+		scores[0][0] = 90;
+		scores[1][1] = 80;
+		
+		for(int i=0; i<scores.length; i++) {
+			for(int j=0; j<scores[i].length; j++) {
+				System.out.println("score[" + i + "]["+ j +"]:" + scores[i][j]);
+			}
+		}
+
+	}
+
+}
