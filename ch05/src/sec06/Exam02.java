8d12eb893a291953cfd3d08f4e3518dcd144c956
choiala <73679392+choiala@users.noreply.github.com>
2020-12-16 17:31:56 +0900

    Create Exam02.java

---
+++ ch05/src/sec06/Exam02.java
@@ -0,0 +1,18 @@
+package sec06;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		//int[] scores = new int[5];
+		//double[] scores = new double[5];
+		//boolean[] scores = new boolean[5];
+		String[] scores = new String[5];
+		
+		for(int i=0; i<scores.length; i++) {
+			System.out.print(scores[i] + ", ");
+		}
+		System.out.println();
+
+	}
+
+}
