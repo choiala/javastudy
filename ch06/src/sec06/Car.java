0ea396577ec26350fefe55a30628b122adb20829
choiala <73679392+choiala@users.noreply.github.com>
2020-12-18 17:07:46 +0900

    Create Car.java

---
+++ ch06/src/sec06/Car.java
@@ -0,0 +1,14 @@
+package sec06;
+
+public class Car {
+	//Field
+	String company = "현대"; //배열처럼 기본값으로 0의 의미의 값이 들어감 
+	int speed;
+	Tire tire = new Tire(); //Tire 객체를 만들어서 tire변수에 번지를 넣는다
+	boolean navi = true;
+	
+	//Constructor
+	//public Car() {} //실행 시 생성자 없으면 기본적으로 호출 됨
+	
+	//Method
+}
